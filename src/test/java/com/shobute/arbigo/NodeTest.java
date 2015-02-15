87a081d7b06585baee189f82ddb23b7961a5a8a4
Ben Lloyd <ben@shobute.com>
2015-02-15 17:53:02 +0000

    Added a couple of node tests.

---
+++ src/test/java/com/shobute/arbigo/NodeTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Ben Lloyd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.shobute.arbigo;
+
+import com.shobute.arbigo.common.Node;
+import java.awt.Point;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Ben Lloyd
+ */
+public class NodeTest {
+    
+    @Test
+    public void equality() {
+        Point point = new Point();
+        Node node1 = new Node(point);
+        Node node2 = new Node(point);
+        Assert.assertNotEquals(node1, node2);
+        
+        int hash1 = node1.hashCode();
+        node1.setLocation(9, 9);
+        int hash2 = node1.hashCode();
+        Assert.assertEquals(hash1, hash2);
+    }
+    
+    @Test
+    public void adjacentNodes() {
+        Point point = new Point();
+        Node node1 = new Node(point);
+        Node node2 = new Node(point);
+        Assert.assertTrue(node1.addAdjacentNode(node2));
+        Assert.assertFalse(node1.addAdjacentNode(node2));
+        Assert.assertTrue(node1.getAdjacentNodes().size() == 1);
+    }
+    
+}
